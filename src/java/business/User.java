48a38f363fb1705ccc3afe90d0e1dcf0cb17ec79
tl571727 <tl571727@LR002D06.southeast.edu>
2023-09-18 14:02:34 -0500

    created a user class and started student page

--- src/java/business/User.java
+++ src/java/business/User.java
@@ -4,10 +4,57 @@
  */
 package business;
 
+import java.io.Serializable;
+
 /**
  *
  * @author tmdel
  */
-public class User {
+public class User implements Serializable{
+    private int userID;
+    private String username, password, email;
+
+    public User() {
+        
+    }
+
+    public User(int userID, String username, String password, String email) {
+        this.userID = userID;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
     
 }
