36641b660cce8c43af0029cb8ecba3d1ac83964d
dt655510 <dt655510@LR002D04.southeast.edu>
2023-09-20 14:31:37 -0500

    added parent and inserts

--- src/java/data/MathDB.java
+++ src/java/data/MathDB.java
@@ -27,7 +27,7 @@ public class MathDB {
         PreparedStatement ps = null;
 
         String query
-                = "INSERT INTO user (userID, username, password, firstName, lastName, email, userType) "
+                = "INSERT INTO users (userID, username, password, firstName, lastName, email, userType) "
                 + "VALUES (?, ?, ?, ?, ?, ?, ?)";
         try {
             ps = connection.prepareStatement(query);
@@ -83,5 +83,98 @@ public class MathDB {
                 throw e;
             }
         }
+    }
+    
+    public static LinkedHashMap<String, User> selectAllUsers() throws SQLException {
+        ConnectionPool pool = ConnectionPool.getInstance();
+        Connection connection = pool.getConnection();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        String query = "SELECT * FROM users";
+        try {
+            ps = connection.prepareStatement(query);
+
+            rs = ps.executeQuery();
+            User user = null;
+            LinkedHashMap<String, User> users = new LinkedHashMap();
+
+            while (rs.next()) {
+                int userID = rs.getInt("userID");
+                String username = rs.getString("username");
+                String password = rs.getString("password");
+                String firstName = rs.getString("firstName");
+                String lastName = rs.getString("lastName");
+                String email = rs.getString("email");
+                String userType = rs.getString("userType");
+                user = new User(userID, username, password, firstName, lastName, email, userType);
+                users.put(user.getUsername(), user);
+            }
+            return users;
+        } catch (SQLException e) {
+            LOG.log(Level.SEVERE, "*** select all sql", e);
+            throw e;
+        } finally {
+            try {
+                rs.close();
+                ps.close();
+                pool.freeConnection(connection);
+            } catch (Exception e) {
+                LOG.log(Level.SEVERE, "*** select all null pointer?", e);
+                throw e;
+            }
+        }
+    }
+    
+    public static String getPasswordForUsername(String username) {
+        LinkedHashMap<String, User> users = new LinkedHashMap();
+        try {
+            users = MathDB.selectAllUsers();
+        } catch (Exception e) {
+
+        }
+
+        if (users.containsKey(username)) {
+            User user = users.get(username);
+            String password = user.getPassword();
+            return password;
+        } else {
+            return null;
+        }
+        
+        
+    }
+    
+    public static int selectUserID(String username) throws SQLException {
+        ConnectionPool pool = ConnectionPool.getInstance();
+        Connection connection = pool.getConnection();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        
+        String query
+                = "SELECT userID "
+                + "FROM users "
+                + "WHERE username = ?";
+        try {
+            ps = connection.prepareStatement(query);
+            ps.setString(1, username);
+            rs = ps.executeQuery();
+            int userID = 0;
+            if (rs.next()) {
+                userID = rs.getInt("userID");
+            }
+            return userID;
+        } catch (SQLException e) {
+            LOG.log(Level.SEVERE, "*** select userID", e);
+            throw e;
+        } finally {
+            try {
+                ps.close();
+                rs.close();
+                pool.freeConnection(connection);
+            } catch (Exception e) {
+                LOG.log(Level.SEVERE, "*** select userID null pointer?", e);
+            }
+        } 
     }
 }
